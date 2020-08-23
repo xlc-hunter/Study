347eed46e84455d583db2c7e14eec134b07daad5
Hunter <790625693@qq.com>
2020-08-23 13:27:15 +0800

    python3B 仓库第二次提交

--- firsttest.py
+++ firsttest.py
@@ -13,11 +13,11 @@ i = 1
 cap = cv2.VideoCapture(0)   # 打开笔记本的内置摄像头  参数是视频文件路径则打开视频
 while 1:
     ret, frame = cap.read()  # 按帧读取图片  返回值ret代表是否读取 成功    frame 代表这一帧图像
-    cv2.imshow('cap', frame)
-    flag = cv2.waitKey(1)
+    cv2.imshow('cap', frame) # 用一个窗口显示 cap 图片   frame 是显示视频 
+    flag = cv2.waitKey(1)  # 加上键盘控制键 
     if flag == 13: # 按下回车键
-        output_path = os.path.join(output_dir,  "%04d.jpg" % i)
-        cv2.imwrite(output_path, frame)
+        output_path = os.path.join(output_dir,  "%04d.jpg" % i)  # 连接两个或更多的路径名组件
+        cv2.imwrite(output_path, frame)  
         i += 1
     if flag == 27:  # 按下ESC键
         break
